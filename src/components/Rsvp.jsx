de7b62ba854ec634453909edbd43d6bd6cb048d2
HARSH JHAVERI <harshjhaveri@HARSHs-MacBook-Pro.local>
2023-05-19 00:18:40 -0400

    Changed RSVP

--- src/components/Rsvp.jsx
+++ src/components/Rsvp.jsx
@@ -1,37 +1,29 @@
+/* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from 'react';
 import {
-  Flex, Box, Input, InputGroup, IconButton, Link,
+  Flex, Box, Input, InputGroup, IconButton, Link, Divider, Text,
 } from '@chakra-ui/react';
 import { ArrowBackIcon, SearchIcon } from '@chakra-ui/icons';
 import RsvpCard from './RsvpCard';
-import { H1, H2 } from './Typography';
+import { H1, H2, H3 } from './Typography';
 import { editAttendeeStatus, getAllAttendees, searchForAttendee } from '../api';
 
 export default function Rsvp() {
-  const [dad, setDad] = useState([]);
-  const [mom, setMom] = useState([]);
+  const [attending, setAttending] = useState([]);
+  const [searched, setSearch] = useState([]);
   // eslint-disable-next-line no-unused-vars
   const [query, setQuery] = useState('');
-
-  const createArrays = (response) => {
-    const dadArr = [];
-    const momArr = [];
-    for (const attendee of response) {
-      if (attendee.family === 'mom') {
-        momArr.push(attendee);
-      } else {
-        dadArr.push(attendee);
-      }
-    }
-
-    setDad(dadArr);
-    setMom(momArr);
-  };
 
   useEffect(() => {
     async function fetch() {
       const response = await getAllAttendees();
-      createArrays(response);
+      const arr = [];
+      for (const person of response) {
+        if (person.attending === 1) {
+          arr.push(person.name);
+        }
+      }
+      setAttending(arr);
     }
 
     fetch();
@@ -40,18 +32,16 @@ export default function Rsvp() {
   const searchForAttendees = async (q) => {
     // eslint-disable-next-line quotes
     const res = await searchForAttendee(q);
-    createArrays(res);
+    setSearch(res);
     console.log(res);
   };
 
-  const setDadAttending = async (id, attending) => {
-    setDad(dad.map((d) => (d.id === id ? { ...d, attending } : d)));
-    await editAttendeeStatus(id, attending);
-  };
-
-  const setMomAttending = async (id, attending) => {
-    setMom(mom.map((m) => (m.id === id ? { ...m, attending } : m)));
-    await editAttendeeStatus(id, attending);
+  const setAttendeeStatus = async (id, attend) => {
+    setSearch(searched.map((p) => (p.id === id ? { ...p, attending: attend } : p)));
+    const attendee = await editAttendeeStatus(id, attend);
+    if (attend === 1) {
+      setAttending([...attending, attendee.name]);
+    }
   };
 
   return (
@@ -60,27 +50,21 @@ export default function Rsvp() {
         <ArrowBackIcon m={[1, 5]} style={{ position: 'absolute', top: '0px', left: '0px' }} boxSize={[6, 8]} />
       </Link>
       <H1>RSVP</H1>
+      <H3>By May 31st</H3>
       <InputGroup m={2} display="flex" justifyContent="center">
         <Input w="50%" onChange={(event) => { setQuery(event.target.value); }} value={query} placeholder="search for your name" />
         <IconButton onClick={() => searchForAttendees(query)} icon={<SearchIcon />} />
       </InputGroup>
       <Flex w="100%" bgColor="white" justifyContent="space-around" p={30}>
-        <Box w={['50%', '33%']} h="auto">
-          <H2>India</H2>
+        <Box w={['90%', '75%']} h="auto">
           <Flex flexDir="column" w="100%" justifyContent="center">
-            {dad.map((d) => {
-              return (
-                <RsvpCard key={d.id} id={d.id} name={d.name} attending={d.attending} set={setDadAttending} />
-              );
-            })}
+            {searched.map((person) => <RsvpCard key={person.id} id={person.id} name={person.name} attending={person.attending} set={setAttendeeStatus} />)}
+            <Divider />
+            <H2>Who&apos;s Coming</H2>
+            {attending.map((person) => <Text fontSize={['2xl', '3xl']} key={person}>{person}</Text>)}
           </Flex>
         </Box>
-        <Box w={['50%', '33%']}>
-          <H2>US</H2>
-          <Flex flexDir="column" w="100%" justifyContent="center">
-            {mom.map((m) => <RsvpCard key={m.id} id={m.id} name={m.name} attending={m.attending} set={setMomAttending} />)}
-          </Flex>
-        </Box>
+
       </Flex>
 
     </Flex>
